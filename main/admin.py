6a66ba2d9d23714d3726b8c9ac96ba71cf3c320c
Isaac Dempsey <idempsey7@gmail.com>
2018-06-15 16:00:38 +0100

    Added DublinBusStops table and registered models at admin.py

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Leavetimes, Trips, DublinBusStops
 
 # Register your models here.
+admin.site.register(Leavetimes)
+admin.site.register(Trips)
+admin.site.register(DublinBusStops)
