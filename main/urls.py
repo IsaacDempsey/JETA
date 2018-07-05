612f1f81ca652609d283fbb1272ebef259c16197
Isaac Dempsey <idempsey7@gmail.com>
2018-07-05 14:18:30 +0100

    Added example of use of pandas in views

--- main/urls.py
+++ main/urls.py
@@ -5,7 +5,6 @@ app_name='main'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('stations', views.stations, name='stations'),
     path('get_address', views.get_address, name='get_address'),
     path('linked', views.linked, name='linked'),
     path('stops', views.stops, name='stops')
--- main/views.py
+++ main/views.py
@@ -1,33 +1,32 @@
-from django.shortcuts import render
+from django.db import connection
 from django.http import JsonResponse, HttpResponse
-import json
+from django.shortcuts import render
 from django.urls import reverse
-<<<<<<< HEAD
-from .models import Stops
-import pandas as pd
-
-=======
 from .models import Stops, Linked, Routes
->>>>>>> f42b75be682c098932543c95248ec6085ba72d53
+
+import json
+import pandas as pd
 
 def index(request):
     return render(request, 'index.html')
 
-# Returns first 10 stations in Stops table as JSON.
-def stations(request):
-    stations = Stations.objects.all()[:10].values()
-    print(stations)
-
-    stationJson = []
-    for i in stations:
-        stationJson.append(dict(i))
-
-    return JsonResponse(stationJson, safe=False)
-
 def stops(request):
-    stops = pd.DataFrame(list(Stops.objects.all()[:10].values()))
-    
-    return stops.to_json(orient='split')
+    stops = Stops.objects.all()[:10].values()
+
+    stops_df = pd.DataFrame.from_records(stops)
+
+    # Test that stop df contains data in server terminal.
+    print(stops_df.head(5))
+
+    return HttpResponse(stops_df.to_json())
+
+    # Alternative code - building json from list of dicts.
+
+    # stopsJson = []
+    # for i in stops:
+    #     stopsJson.append(dict(i))
+
+    # return JsonResponse(stopsJson, safe=False)
 
 
 def get_address(request):
