3166601e943b27c5c5444a6d131b9243c0d8165c
Harsh Gupta <harshrp94@gmail.com>
2018-07-23 15:20:14 +0100

    Setting up wsgi and gunicorn urlpatterns'

--- jeta/urls.py
+++ jeta/urls.py
@@ -18,9 +18,12 @@ from django.urls import *
 from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('main/', include('main.urls')),
     path('admin/', admin.site.urls),
     path('', RedirectView.as_view(url='/main/', permanent=True)),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
