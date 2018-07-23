a7838be06e7c7134dccec36f82aa2ed3d4dd2644
Harsh Gupta <harshrp94@gmail.com>
2018-07-23 16:29:51 +0100

    Resolving merge errors

--- jeta/urls.py
+++ jeta/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     path('main/', include('main.urls')),
     path('admin/', admin.site.urls),
     path('', RedirectView.as_view(url='/main/', permanent=True)),
-] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
+] 
 
-# urlpatterns += staticfiles_urlpatterns()
+urlpatterns += staticfiles_urlpatterns()
